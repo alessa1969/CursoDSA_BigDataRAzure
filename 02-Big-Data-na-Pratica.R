ef2d3a40dd4339d301ea5a5b2811cee8f5d1b824
alessa.emcomum@gmail.com <alessa.emcomum@gmail.com>
2021-12-11 13:15:24 -0300

    Estudos das temperaturas medias das cidades do Brasil

---
+++ 02-Big-Data-na-Pratica.R
@@ -0,0 +1,109 @@
+# Big Data na Prática 1 - Analisando a temperatura Média nas Cidades Brasileiras
+
+#Configurando o diretório de trabalho
+getwd()
+setwd("C:/DSA/FCD/BigDataRAzure/CAP03")
+getwd()
+
+# Dataset
+# Berkeley Earth
+# https://berkeleyearth.org/data/
+# http://berkeleyearth.org/data/
+# TemperaturasGlobais.csv ~ 78 MB (zip) (unzip)
+# Faça o download do arquivo zip no link abaizxo e descompacte na mesma pasta deste script
+# https://drive.google.com/file/d/1nSwP3Y0V7gncbnG_DccNhrTRxmUNqMqa/view
+
+# Instalando e Carregando Pacotes
+# Obs: Os pacotes precisam ser instalados apenas uma vez. Se já instalou em outros scripts, não precisa repetir o procedimento abaixo
+install.packages("readr")
+install.packages("data.table")
+install.packages("dplyr")
+install.packages("ggplot2")
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(data.table)
+
+# Carregando os dados (Usando um timer para comparar o tempo de carregamento com diferentes)
+
+# Usando read.csv2()
+system.time(df_teste1 <- read.csv2("TemperaturasGlobais.csv"))
+
+# Usando read.table()
+system.time(df_teste2 <- read.table("TemperaturasGlobais.csv"))
+
+# Usando fread()
+?fread()
+system.time(df <- fread("TemperaturasGlobais.csv"))
+
+# Criando subsets dos dados carregados
+cidadesBrasil <- subset(df, Country =='Brazil')
+cidadesBrasil <- na.omit(cidadesBrasil)
+head(cidadesBrasil)
+nrow(df)
+nrow(cidadesBrasil)
+dim(cidadesBrasil)
+
+# Preparação e Organização
+
+# Convertendo as Datas
+cidadesBrasil$dt <- as.POSIXct(cidadesBrasil$dt, format='%Y-%m-%d')
+cidadesBrasil$Month <- month(cidadesBrasil$dt)
+cidadesBrasil$Year <- year(cidadesBrasil$dt)
+head(cidadesBrasil)
+
+# Carregando os subsets
+
+# Palmas
+plm <- subset(cidadesBrasil, City == 'Palmas')
+plm <- subset(plm, Year %in% c(1796, 1846, 1896, 1946, 1996, 2012))
+
+# Curitiba
+crt <- subset(cidadesBrasil, City == 'Curitiba')
+crt <- subset(crt, Year %in% c(1796, 1846, 1896, 1946, 1996, 2012))
+
+# Recife
+recf <- subset(cidadesBrasil, City == 'Recife')
+recf <- subset(recf, Year %in% c(1796, 1846, 1896, 1946, 1996, 2012))
+
+# Construindo os Plots
+
+p_plm <- ggplot(plm, aes(x = (Month), y = AverageTemperature, color = as.factor(Year))) +
+  geom_smooth(se = FALSE, fill = NA, size =2) +
+  theme_light(base_size = 20) +
+  xlab("Mês") +
+  ylab("Temperatura Média") +
+  scale_color_discrete("") +
+  ggtitle("Temperatura Media ao longo dos anos em Palmas") +
+  theme(plot.title = element_text(size = 18))
+
+p_crt <- ggplot(crt, aes(x = (Month), y = AverageTemperature, color = as.factor(Year))) +
+  geom_smooth(se = FALSE, fill = NA, size =2) +
+  theme_light(base_size = 20) +
+  xlab("Mês") +
+  ylab("Temperatura Média") +
+  scale_color_discrete("") +
+  ggtitle("Temperatura Media ao longo dos anos em Curitiba") +
+  theme(plot.title = element_text(size = 18))
+
+p_recf <- ggplot(recf, aes(x = (Month), y = AverageTemperature, color = as.factor(Year))) +
+  geom_smooth(se = FALSE, fill = NA, size =2) +
+  theme_light(base_size = 20) +
+  xlab("Mês") +
+  ylab("Temperatura Média") +
+  scale_color_discrete("") +
+  ggtitle("Temperatura Media ao longo dos anos em Recife") +
+  theme(plot.title = element_text(size = 18))
+
+
+# Plotando
+p_plm
+p_crt
+p_recf
+
+
+
+
+
+
